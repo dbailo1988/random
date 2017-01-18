a944455d52fc89b897907d438c1d8e0faa293a99
Daniel Bailo <daniel@ip-192-168-0-53.eu-west-1.compute.internal>
2017-01-18 10:32:36 +0200

    Cambiar string

--- random.js
+++ random.js
@@ -1,10 +1,10 @@
 // Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
-var str =' MAYOR que 0,5';
+var str =' es mayor que 0,5';
 
 if (numero <= 0.5){
-  str = ' MENOR que 0,5';
+  str = ' es menor que 0,5';
 }
 
 console.log('\n' + numero + str + '\n');
